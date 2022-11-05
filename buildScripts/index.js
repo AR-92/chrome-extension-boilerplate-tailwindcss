ec941c5a6919522c999c9be03fa0c7517f9a357c
AR-92 <muhammadahmedrana1992@yahoo.com>
2022-11-05 05:26:45 +0500

    dist removed

--- buildScripts/index.js
+++ buildScripts/index.js
@@ -1,23 +1,29 @@
 const fs = require("fs")
 const path = require("path")
-var allfiles=[
-    ["./src/background", "./dist/background"],
-    ["./src/content", "./dist/content"],
-    ["./src/popup", "./dist/popup"],
-    ["./src/assets/js", "./dist//assets/js"],
-    ["./src/assets/img", "./dist//assets/img"],
-    // ["./src/manifest.json", "./dist/manifest.json"],
-]
-function copyFolderSync(from, to) {
-    fs.mkdirSync(to);
-    fs.readdirSync(from).forEach(element => {
-        if (fs.lstatSync(path.join(from, element)).isFile()) {
-            fs.copyFileSync(path.join(from, element), path.join(to, element));
-        } else {
-            copyFolderSync(path.join(from, element), path.join(to, element));
-        }
-    });
-}
-allfiles.forEach(f=>{
-    copyFolderSync(f[0], f[1])
-})
+// var allfiles=[
+//     ["./src/background", "./dist/background"],
+//     ["./src/content", "./dist/content"],
+//     ["./src/popup", "./dist/popup"],
+//     ["./src/assets/js", "./dist//assets/js"],
+//     ["./src/assets/img", "./dist//assets/img"],
+//     // ["./src/manifest.json", "./dist/manifest.json"],
+// ]
+// function copyFolderSync(from, to) {
+//     fs.readdirSync(from).forEach(element => {
+//         if (fs.lstatSync(path.join(from, element)).isFile()) {
+//             fs.copyFileSync(path.join(from, element), path.join(to, element));
+//         } else {
+//             fs.mkdirSync(to);
+//             copyFolderSync(path.join(from, element), path.join(to, element));
+//         }
+//     });
+// }
+// allfiles.forEach(f=>{
+//     copyFolderSync(f[0], f[1])
+// })
+fs.watch('./src/', (eventType, filename) => {
+    console.log(eventType);
+    // could be either 'rename' or 'change'. new file event and delete
+    // also generally emit 'rename'
+    console.log(filename);
+    })
